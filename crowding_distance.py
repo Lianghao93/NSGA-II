59f5740b71b9f7d07fee21269bda46688af73d13
ChengHust <chenghehust@gmail.com>
2017-08-10 16:38:06 +0800

    Update crowding_distance.py

--- crowding_distance.py
+++ crowding_distance.py
@@ -3,16 +3,16 @@
 import numpy as np
 
 def crowding_distance(pop_obj, front_no):
-    (n, m) = np.shape(pop_obj)
+    n, m_obj = np.shape(pop_obj)
     crowd_dis = np.zeros(n)
     front = np.unique(front_no)
     Fronts = front[front != np.inf]
     for f in range(len(Fronts)):
-        Front = front_no == Fronts[f]
+        Front = np.nonzero(front_no == Fronts[f])[0]
         Fmax = pop_obj[Front, :].max(0)
         Fmin = pop_obj[Front, :].min(0)
-        for i in range(m):
-            rank = np.argsort(pop_obj[Front, i])
+        for i in range(m_obj):
+            rank = np.argsort(pop_obj[Front,i])
             crowd_dis[Front[rank[0]]] = np.inf
             crowd_dis[Front[rank[-1]]] = np.inf
             for j in range(1, len(Front) - 1):
