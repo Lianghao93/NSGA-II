3e397514880c8bf2a1aa611c93be26b9cf483432
ChengHust <chenghehust@gmail.com>
2017-08-10 15:23:50 +0800

    Update nsgaii.py
    Add a draw function to visualize the non-dominated solutions.

--- nsgaii.py
+++ nsgaii.py
@@ -5,6 +5,8 @@ from tournament import tournament
 from environment_selection import environment_selection
 from GLOBAL import Global
 
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 Global = Global(lower=-np.ones((1,10)),upper=np.ones((1,10)))
@@ -24,12 +26,28 @@ class nsgaii(object):
         front_no, max_front = nd_sort(population[1], np.inf)
         crowd_dis = crowding_distance(population[1], front_no)
         while self.eva>=0:
-            mating_pool = tournament(2,Global.N,front_no, crowd_dis)
+            mating_pool = tournament(2 ,Global.N, front_no, crowd_dis)
             parent = [population[0][mating_pool], population[1][mating_pool]]
             offspring = Global.variation(parent[0])
             population = Global.unit_population(population, offspring)
-            population = environment_selection(population,Global.N)
+            population = environment_selection(population, Global.N)
         return population
+    
+    def draw(self):
+        population = self.run()
+        pop_obj = population[1]
+        front_no, max_front = nd_sort(pop_obj, 1)
+        non_dominated = pop_obj[front_no==1, :]
+        if Global.m == 2:
+            plt.scatter(non_dominated[0, :], non_dominated[1, :])
+        elif Global.m == 3:
+            x,y,z = non_dominated[0, :], non_dominated[1, :],non_dominated[2, :]
+            ax = plt.subplot(111,projection='3d') 
+            ax.scatter(x, y,z,c='b') 
+        else:
+            for i in range(len(non_dominated)):
+                plt.plot(range(1, Global.m + 1), non_dominated[i, :])
+         
    
 
 
