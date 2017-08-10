1a33aaecb21d542f024394ed506f84aeefa2c6d5
ChengHust <chenghehust@gmail.com>
2017-08-10 16:21:48 +0800

    Update nd_sort.py

--- nd_sort.py
+++ nd_sort.py
@@ -10,12 +10,12 @@ def nd_sort(pop_obj, n_sort):
     :return: [FrontNo, MaxFNo]
     '''
     n, m_obj = np.shape(pop_obj)
-    a, loc = np.unique(pop_obj[(:, 0)], return_inverse = True)
+    a, loc = np.unique(pop_obj[:, 0], return_inverse = True)
     index = pop_obj[:, 0].argsort()
     new_obj = pop_obj[index, :]
     front_no = np.inf * np.ones(n)
     max_f_no = 0
-    while np.sum(front_no < np.inf) < min(n_sort, n):
+    while np.sum(front_no < np.inf) < min(n_sort, len(loc)):
         max_f_no += 1
         for i in range(n):
             if front_no[i] == np.inf:
@@ -26,9 +26,8 @@ def nd_sort(pop_obj, n_sort):
                         while m <= m_obj and new_obj[i][m - 1] >= new_obj[j][m - 1]:
                             m += 1
                         dominated = m > m_obj
-                        if not dominated:
-                            if m_obj == 2:
-                                break
-                            if not dominated:
-                                front_no[i] = max_f_no
+                        if not dominated or m_obj ==2:
+                            break
+                if not dominated:
+                    front_no[i] = max_f_no
     return front_no[loc], max_f_no
