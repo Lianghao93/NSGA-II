39f9a1f288eb1aec12e168f5131deab0fb9ac5bc
ChengHust <chenghehust@gmail.com>
2017-08-10 19:42:15 +0800

    Update environment_selection.py

--- environment_selection.py
+++ environment_selection.py
@@ -15,9 +15,9 @@ def environment_selection(population, N):
     front_no, max_front = nd_sort(population[1], N)
     next_label = front_no < max_front
     crowd_dis = crowding_distance(population[1], front_no)
-    last = front_no == max_front
+    last = np.array([i for i in range(len(front_no)) if front_no[i]==max_front])
     rank = np.argsort(-crowd_dis[last])
-    delta_n = rank[:N - next_label.sum()]
+    delta_n = rank[: (N - next_label.sum())]
     next_label[last[delta_n]] = True
     next_pop = [population[0][next_label], population[1][next_label]]
     return next_pop, front_no, crowd_dis
