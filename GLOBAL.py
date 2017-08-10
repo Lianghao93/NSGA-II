bc46015795991585c5acb86747ee7422302d8ee1
ChengHust <chenghehust@gmail.com>
2017-08-10 17:04:47 +0800

    Update GLOBAL.py

--- GLOBAL.py
+++ GLOBAL.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+import numpy as np
 class Global(object):
     
     def __init__(self, d, n, M, upper, lower):
@@ -14,16 +16,13 @@ class Global(object):
         :param x: the decision vectors
         :return: the objective vectors
         '''
-        a1 = np.random.randn(1, self.d)
-        a2 = np.random.randn(1, self.d)
-        f1 = np.dot(a1, np.power(x[(0, :)], 2).T)
-        f2 = np.dot(a2, -(np.power(x[(0, :)], 3).T))
-        obj = np.hstack((f1, f2))
-        for i in range(1, len(x)):
-            f1 = np.dot(a1, np.power(x[(i, :)], 2).T)
-            f2 = np.dot(a2, -(np.power(x[(i, :)], 3).T))
-            obj = np.vstack((obj, np.hstack((f1, f2))))
-        return np.array(obj)
+        n = x.shape[0]
+        a = np.random.randn(self.M, self.d)
+        obj = np.zeros((n,self.M))
+        for i in range(n):
+            for j in range(self.M):
+                obj[i,j] = np.dot(x[i,:]**(j+1),a[j,:].T)
+        return obj
 
     
     def individual(self, decs):
@@ -70,13 +69,13 @@ class Global(object):
         dis_m = 20
         pop_dec = pop_dec[:(len(pop_dec) // 2) * 2][:]
         (n, d) = np.shape(pop_dec)
-        parent_1_dec = pop_dec[(:n // 2, :)]
-        parent_2_dec = pop_dec[(n // 2:, :)]
+        parent_1_dec = pop_dec[:n // 2, :]
+        parent_2_dec = pop_dec[n // 2:, :]
         beta = np.zeros((n // 2, d))
         mu = np.zeros((n // 2, d))
         beta[mu <= 0.5] = np.power(2 * mu[mu <= 0.5], 1 / (dis_c + 1))
         beta[mu > 0.5] = np.power(2 * mu[mu > 0.5], -1 / (dis_c + 1))
-        beta = beta * -1 * np.random.randint(2, (n // 2, d), **None)
+        beta = beta * -1 * np.random.randint(2, size=(n // 2, d))
         beta[np.random.random((n // 2, d)) < 0.5] = 1
         beta[np.tile(np.random.random((n // 2, 1)) > pro_c, (1, d))] = 1
         offspring_dec = np.vstack(((parent_1_dec + parent_2_dec) / 2 + beta * (parent_1_dec - parent_2_dec) / 2, (parent_1_dec + parent_2_dec) / 2 - beta * (parent_1_dec - parent_2_dec) / 2))
@@ -86,9 +85,11 @@ class Global(object):
         lower = np.tile(self.lower, (n, 1))
         upper = np.tile(self.upper, (n, 1))
         norm = (offspring_dec[temp] - lower[temp]) / (upper[temp] - lower[temp])
-        offspring_dec[temp] += (upper[temp] - lower[temp]) * (np.power(2 * mu[temp] + (1 - 2 * mu[temp]) * np.power(1 - norm, dis_m + 1), 1 / (dis_m + 1)) - 1)
+        offspring_dec[temp] += (upper[temp] - lower[temp]) * \
+                               (np.power(2. * mu[temp] + (1. - 2. * mu[temp]) * np.power(1. - norm, dis_m + 1.), 1./(dis_m + 1)) -1.)
         temp = site & (mu > 0.5)
         norm = (upper[temp] - offspring_dec[temp]) / (upper[temp] - lower[temp])
-        offspring_dec[temp] += (upper[temp] - lower[temp]) * (1 - np.power(2 * (1 - mu[temp]) + 2 * (mu[temp] - 0.5) * np.power(1 - norm, dis_m + 1), 1 / (dis_m + 1)))
+        offspring_dec[temp] += (upper[temp] - lower[temp]) * \
+                               (1. - np.power(2. * (1. - mu[temp]) + 2. * (mu[temp] - 0.5) * np.power(1. - norm, dis_m + 1.), 1./(dis_m + 1.)))
         offspring_dec = np.maximum(np.minimum(offspring_dec, upper), lower)
         return self.individual(offspring_dec)
