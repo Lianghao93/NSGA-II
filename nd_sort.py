c13c9583c49810c2e9e0cfc385217698a5131749
ChengHust <chenghehust@gmail.com>
2017-08-10 14:54:50 +0800

    Update nd_sort.py
    An efficient approach to nondominated sorting for evolutionary multiobjective optimization

--- nd_sort.py
+++ nd_sort.py
@@ -1,34 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import numpy as np
+
 def nd_sort(pop_obj, n_sort):
     '''
-    :param pop_obj: the set
-    :param k: number of solutions before the maximum Pareto front
+    :param n_sort:
+    :param pop_obj: objective vectors
     :return: [FrontNo, MaxFNo]
     '''
-
-    N, M = np.shape(pop_obj)
-    a, Loc = np.unique(pop_obj[:, 0], return_inverse=True)
+    n, m_obj = np.shape(pop_obj)
+    a, loc = np.unique(pop_obj[(:, 0)], return_inverse = True)
     index = pop_obj[:, 0].argsort()
-    pop_obj = pop_obj[index, :]
-    front_no = np.inf*np.ones(N)
+    new_obj = pop_obj[index, :]
+    front_no = np.inf * np.ones(n)
     max_f_no = 0
-
-    while np.sum(front_no < np.inf) < min(n_sort, N):
-        max_f_no = max_f_no + 1
-        for i in range(N):
+    while np.sum(front_no < np.inf) < min(n_sort, n):
+        max_f_no += 1
+        for i in range(n):
             if front_no[i] == np.inf:
                 dominated = False
-                for j in range(i-1,0,-1):
+                for j in range(i - 1, 0, -1):
                     if front_no[j] == max_f_no:
                         m = 2
-                        while (m <= M) and (pop_obj[i][m-1] >= pop_obj[j][m-1]):
-                            m = m + 1
-                        dominated = m > M
-                        if dominated or M == 2:
-                            break
-                if not dominated:
-                    front_no[i] = max_f_no
-    front_no = front_no[Loc]
-    return front_no, max_f_no
-
-
+                        while m <= m_obj and new_obj[i][m - 1] >= new_obj[j][m - 1]:
+                            m += 1
+                        dominated = m > m_obj
+                        if not dominated:
+                            if m_obj == 2:
+                                break
+                            if not dominated:
+                                front_no[i] = max_f_no
+    return front_no[loc], max_f_no
