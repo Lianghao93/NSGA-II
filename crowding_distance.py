da2102744f1bda8c53fdc95f0ddcac39c589e2d4
ChengHust <chenghehust@gmail.com>
2017-08-18 16:07:06 +0800

    Update crowding_distance.py

--- crowding_distance.py
+++ crowding_distance.py
@@ -2,19 +2,27 @@
 # encoding: utf-8
 import numpy as np
 
+
 def crowding_distance(pop_obj, front_no):
-    n, m_obj = np.shape(pop_obj)
+    """
+    The crowding distance of each Pareto front
+    :param pop_obj: objective vectors
+    :param front_no: front numbers
+    :return: crowding distance
+    """
+    n, M = np.shape(pop_obj)
     crowd_dis = np.zeros(n)
     front = np.unique(front_no)
     Fronts = front[front != np.inf]
     for f in range(len(Fronts)):
-        Front = np.array([k for k in len(front_no) if front_no[k] == Fronts[f]])
+        Front = np.array([k for k in range(len(front_no)) if front_no[k] == Fronts[f]])
         Fmax = pop_obj[Front, :].max(0)
         Fmin = pop_obj[Front, :].min(0)
-        for i in range(m_obj):
-            rank = np.argsort(pop_obj[Front,i])
+        for i in range(M):
+            rank = np.argsort(pop_obj[Front, i])
             crowd_dis[Front[rank[0]]] = np.inf
             crowd_dis[Front[rank[-1]]] = np.inf
             for j in range(1, len(Front) - 1):
-                crowd_dis[Front[rank[j]]] = crowd_dis[Front[rank[j]]] + (pop_obj[(Front[rank[j + 1]], i)] - pop_obj[(Front[rank[j - 1]], i)]) / (Fmax[i] - Fmin[i])
+                crowd_dis[Front[rank[j]]] = crowd_dis[Front[rank[j]]] + (pop_obj[(Front[rank[j + 1]], i)] - pop_obj[
+                    (Front[rank[j - 1]], i)]) / (Fmax[i] - Fmin[i])
     return crowd_dis
